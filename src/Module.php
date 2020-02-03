0b056e28e16964269380184f8b61e765da10f162
laborci <gergely@laborci.hu>
2020-02-03 12:42:56 +0100

    a

--- src/Module.php
+++ src/Module.php
@@ -8,11 +8,9 @@ class Module implements ModuleInterface{
 	public function load($moduleConfig){
 		EventManager::listen(Twigger::EVENT_TWIG_ENVIRONMENT_CREATED, function () use ($moduleConfig){
 			Twigger::Service()->addPath(__DIR__ . '/@resource/', 'smartpage');
-			if (is_array($moduleConfig) && array_key_exists('twig-sources', $moduleConfig)) foreach ($moduleConfig['twig-sources'] as $namespace => $source){
-				Twigger::Service()->addPath($source, $namespace);
-			}
-			if(array_key_exists('twig-source', $moduleConfig)){
-				Twigger::Service()->addPath($moduleConfig['twig-source'], '__main__');
+			if (is_array($moduleConfig)){
+				if (array_key_exists('twig-sources', $moduleConfig)) foreach ($moduleConfig['twig-sources'] as $namespace => $source) Twigger::Service()->addPath($source, $namespace);
+				if (array_key_exists('twig-source', $moduleConfig)) Twigger::Service()->addPath($moduleConfig['twig-source'], '__main__');
 			}
 		});
 	}
